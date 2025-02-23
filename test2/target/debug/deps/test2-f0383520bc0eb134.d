1c044822e740b90ebd18d2c9c452aa90bb346c8e
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-23 23:43:07 +0530

    test2 done

---
+++ test2/target/debug/deps/test2-f0383520bc0eb134.d
@@ -0,0 +1,5 @@
+E:\Code_Everyday\testing_rust\gitGutter-testing-for-sublime\test2\target\debug\deps\libtest2-f0383520bc0eb134.rmeta: src\main.rs
+
+E:\Code_Everyday\testing_rust\gitGutter-testing-for-sublime\test2\target\debug\deps\test2-f0383520bc0eb134.d: src\main.rs
+
+src\main.rs:
